3d46cfa787d6a2475bd3ee6cb467b3db128d7d57
Thinh Dinh <thinhdn266@gmail.com>
2021-06-12 20:09:29 +0700

    [feature/forum-ui] Add forum components

--- src/features/forum/Forum.tsx
+++ src/features/forum/Forum.tsx
@@ -1,9 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
-import Select, { TOption } from '../../common/components/Select/Select';
-import PageHeader from '../../common/components/PageHeader';
-import ScrollBar from '../../common/components/ScrollBar';
-import MultiToggleSwitch from '../../common/components/MultiToggleSwitch';
+import MultiToggleSwitch from '../../common/components/MultiToggleSwitch'
+import PageHeader from '../../common/components/PageHeader'
+import ScrollBar from '../../common/components/ScrollBar'
+import Select, { TOption } from '../../common/components/Select/Select'
+import Table from '../../common/components/Table'
+import StarRate from '../profile/components/StarRate'
+import { data } from './forumData'
+import type { TForumMember } from './ForumMembers'
+import ForumMembers from './ForumMembers'
+import type { TForumUpdatedNumberProps } from './ForumUpdatedNumber'
+import ForumUpdatedNumber from './ForumUpdatedNumber'
 
 export const Forum: React.FC = () => {
   const mockCategories: TOption[] = [
@@ -11,19 +18,19 @@ export const Forum: React.FC = () => {
     { value: 'Bitcoin Talks', label: 'Bitcoin Talks' },
     { value: 'Ethereum', label: 'Ethereum' },
     { value: 'XRP (Ripple)', label: 'XRP (Ripple)' },
-  ];
+  ]
 
   const mockTags: TOption[] = [
     { value: '', label: 'All' },
     { value: 'Coinbase', label: 'Coinbase' },
     { value: 'Cryptocurrency', label: 'Cryptocurrency' },
     { value: 'Instablockchain', label: 'Instablockchain' },
-  ];
+  ]
 
   // Filters
-  const [category, setCategory] = useState<TOption | null>(mockCategories[0]);
-  const [tag, setTag] = useState<TOption | null>(mockTags[0]);
-  const [selectedItem, setSelectedItem] = useState(0);
+  const [category, setCategory] = useState<TOption | null>(mockCategories[0])
+  const [tag, setTag] = useState<TOption | null>(mockTags[0])
+  const [selectedItem, setSelectedItem] = useState(0)
 
   const filterOptions = [
     {
@@ -38,19 +45,57 @@ export const Forum: React.FC = () => {
       onChange: setTag,
       options: mockTags,
     },
-  ];
+  ]
 
-  const data = [
-    { label: 'Creators' },
-    { label: 'Sellers' },
-    { label: 'Buyers' },
-  ];
+  const routeData = [{ label: 'Latest' }, { label: 'Top' }, { label: 'Categories' }]
 
   const routes = {
-    data,
+    data: routeData,
     activeIndex: selectedItem,
     onToggle: setSelectedItem,
-  };
+  }
+
+  const columns = [
+    {
+      key: 'topic',
+      name: 'Topic',
+    },
+    {
+      key: 'favorite',
+      name: '',
+      custom: (value: string | number) => <StarRate rate={+value || 0} maxRate={1} />,
+    },
+    {
+      key: 'members',
+      name: '',
+      custom: (members: TForumMember[]) => <ForumMembers members={members} />,
+    },
+    {
+      key: 'replies',
+      name: 'Replies',
+      custom: (replies: TForumUpdatedNumberProps) => <ForumUpdatedNumber {...replies} />,
+    },
+    {
+      key: 'views',
+      name: 'Views',
+      custom: (views: TForumUpdatedNumberProps) => <ForumUpdatedNumber {...views} />,
+    },
+    {
+      key: 'activity',
+      name: 'Activity',
+      custom: (activity: string) => <span className='text-gray-a0'>{activity}</span>,
+    },
+  ]
+
+  const table = {
+    columns: columns,
+    data: data,
+    headerTrClasses: 'h-12 text-gray-4a font-extrabold sticky',
+    headerTdClasses: 'first:pl-6 last:pr-6 border-b-1px border-line-default border-solid z-10',
+    bodyTrClasses: 'h-12 hover:bg-line-default text-gray-2d',
+    bodyTdClasses: 'first:rounded-l first:pl-6 last:pr-6 last:rounded-r border-b-1px border-line-default border-solid',
+    bodyClasses: 'overflow-y-scroll',
+  }
 
   return (
     <>
@@ -67,22 +112,19 @@ export const Forum: React.FC = () => {
             <div className='flex'>
               <div className='flex h-full items-center justify-end'>
                 <MultiToggleSwitch
-                  containerClassName='rounded-none border'
+                  containerClassName="rounded-none border-none"
                   {...routes}
                 />
               </div>
             </div>
           </div>
-          <div className='bg-white p-5 rounded-lg'>
-            <div className='flex justify-between pb-25px'>
-              <div className='w-244px'>
-                {/* <Select {...filterOptions} className='w-full' /> */}
-              </div>
+          <div className='bg-white px-7 py-5 rounded-lg'>
+            <div className='space-y-5'>
+              <Table {...table} />
             </div>
-            <div className='space-y-5'>forum</div>
           </div>
         </div>
       </ScrollBar>
     </>
-  );
-};
+  )
+}
